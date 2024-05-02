04985dfbb011171f5083d80e093198bcef0b2782
Matei-Ciofu <mateiciofu@gmail.com>
2024-05-02 08:50:46 +0200

    aaaa

---
+++ endpoints/utenti.js
@@ -0,0 +1,120 @@
+function endpoint(app, connpool) {
+
+    app.post("/api/locali", (req, res) => {
+        var errors = []
+        /* controllo dati inseriti
+        if (!req.body.description) {
+            errors.push("No description specified");
+        }
+        if (req.body.status === "") {
+            errors.push("No status specified");
+        }
+        */
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
+            return;
+        }
+        var data = {
+            nome: req.body.nome,
+            cognome: req.body.cognome,
+        }
+
+        var sql = 'INSERT INTO utente  (nome, cognome) VALUES (?,?)'
+        var params = [data.nome, data.cognome]
+        connpool.query(sql, params, (error, results) => {
+            if (error) {
+                res.status(400).json({ "error": error.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id": this.insertID
+            })
+            console.log(results)
+        });
+
+    })
+
+
+
+    app.get("/api/locali", (req, res, next) => {
+        var sql = "select * from utente"
+        var params = []
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        });
+    });
+
+
+    app.get("/api/Locali/:id", (req, res) => {
+        var sql = "select * from utente where idutente = ?"
+        var params = [req.params.id]
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows[0]
+            })
+        });
+    });
+
+
+    app.put("/api/locali/:id", (req, res) => {
+        var data = {
+            description: req.body.description,
+            status: req.body.status,
+        }
+        connpool.execute(
+            `UPDATE utente set 
+               description = COALESCE(?,description), 
+               status = COALESCE(?,status) 
+               WHERE Locale_id = ?`,
+            [data.description, data.status, req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                console.log(result)
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: result.affectedRows
+                })
+            });
+    })
+
+
+
+    app.delete("/api/Locale/:id", (req, res) => {
+        connpool.execute(
+            'DELETE FROM utente WHERE idutente = ?',
+            [req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                res.json({ "message": "deleted", changes: result.affectedRows })
+            });
+    })
+
+
+}
+
+
+
+
+
+module.exports = endpoint;
